86b6593fef9ec41356900ca654d9cb5776ed13e5
deniswww <dkashapov3@gmail.com>
2026-04-30 11:28:18 +0300

    test

--- reviews-services/tests/conftest.py
+++ reviews-services/tests/conftest.py
@@ -1,7 +1,7 @@
 """
 conftest.py – общие фикстуры для тестов reviews-service.
 
-ВАЖНО: DATABASE_URL подменяется на SQLite ДО импорта app.*,
+ВАЖНО: DATABASE_URL подменяется на SQLite ДО импорта app.*, jjjj
 иначе database.py попытается подключиться к PostgreSQL уже при загрузке. kx kx kx
 """
 
